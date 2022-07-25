7e3f1d471e207bc650b742beabb27e148dfb1a87
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2022-07-25 15:17:51 -0700

    Use SDL2 instead of low level winit.

--- main.rs
+++ main.rs
@@ -1,27 +1,24 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
+// a chomper. Size increases when eating other pacmen.
 struct Pacman {
     x: u32,
     y: u32,
     direction: u8,
     size: u8
 }
-fn main() {
+pub fn main() {
     let player = Pacman {x:1, y:1, direction:1, size:1};
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
 
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
 }
