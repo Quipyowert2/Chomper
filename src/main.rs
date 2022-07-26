8ebfd8bffe78e423c259264118e617769934b739
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2022-07-25 21:52:00 -0700

    Add a draw function.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,21 @@ extern crate sdl2;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::Canvas;
 use std::time::Duration;
 
 // a chomper. Size increases when eating other pacmen.
+#[derive(Clone, Copy)]
 struct Pacman {
     x: u32,
     y: u32,
     direction: u8,
     size: u8
+}
+impl Pacman {
+    fn draw<T>(self: Pacman, canvas: &mut T) {
+        // draw circle with a part missing
+    }
 }
 pub fn main() {
     let player = Pacman {x:1, y:1, direction:1, size:1};
@@ -43,7 +50,7 @@ pub fn main() {
         // The rest of the game loop goes here...
 
         // Draw pacman here.
-        // draw_circle(&canvas);
+        player.draw(&mut canvas);
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
