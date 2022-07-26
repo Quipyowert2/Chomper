d466428327a0d5d1af800d2efd49e9c180a9c36c
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2022-07-26 15:34:07 -0700

    Write function to move the pacman.

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,29 @@ use sdl2::rect::Point;
 use sdl2::video::Window;
 use std::time::Duration;
 
+#[derive(Clone, Copy)]
+enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    UPLEFT,
+    UPRIGHT,
+    DOWN,
+    DOWNLEFT,
+    DOWNRIGHT
+}
 // a chomper. Size increases when eating other pacmen.
 #[derive(Clone, Copy)]
 struct Pacman {
     x: i32,
     y: i32,
-    direction: i8,
-    size: i32
+    direction: Direction,
+    size: i32,
+    color: Color
 }
 impl Pacman {
     fn draw(self: Pacman, canvas: &mut Canvas<Window>) {
+        canvas.set_draw_color(self.color);
         let size_squared = self.size.pow(2);
         // draw circle with a part missing
         for x in self.x-self.size..self.x+self.size {
@@ -28,9 +41,22 @@ impl Pacman {
             }
         }
     }
+    fn move_pacman(&mut self) {
+        match self.direction {
+            Direction::RIGHT => {
+                self.x = self.x + 1 }
+            Direction::LEFT => {self.x = self.x - 1 }
+            Direction::UP => {self.y = self.y - 1 }
+            Direction::UPRIGHT => {self.y = self.y - 1; self.x = self.x + 1}
+            Direction::UPLEFT => {self.y = self.y - 1;self.x = self.x - 1}
+            Direction::DOWN => {self.y = self.y + 1}
+            Direction::DOWNLEFT => {self.y = self.y + 1;self.x = self.x - 1}
+            Direction::DOWNRIGHT => {self.y = self.y + 1;self.x = self.x + 1}
+        }
+    }
 }
 pub fn main() {
-    let player = Pacman {x:1, y:1, direction:1, size:1};
+    let mut player = Pacman {x:400, y:300, direction:Direction::RIGHT, size:40, color:Color::RGB(255,255,0)};
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -62,6 +88,8 @@ pub fn main() {
 
         // Draw pacman here.
         player.draw(&mut canvas);
+        player.move_pacman();
+
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
